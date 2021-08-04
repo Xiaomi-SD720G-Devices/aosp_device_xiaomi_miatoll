ffb6ab4dfd667403314d0eb16804e3d0b7699878
Nick C <pokemonnekitgod@gmail.com>
2021-08-04 17:10:46 +0200

    miatoll: Changes for Pixel thermal HAL
    Change-Id: Id5e4ec3e15f047a6ffd84af34dd9943fa70f7554
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    Signed-off-by: userariii <abhattacharjee717@gmail.com>

--- atoll.mk
+++ atoll.mk
@@ -558,6 +558,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.pixel
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+
 # Touchscreen
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
