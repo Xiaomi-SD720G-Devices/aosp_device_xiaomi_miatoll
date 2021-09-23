b9ac7119d05c8e1a40571afb6130f7179732b66c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-23 16:49:09 +0200

    sm6250-common: Move to common Xiaomi light AIDL
    Change-Id: I00494cd392d19abd97cd65aeb4ec4bd9c9b11352

--- common.mk
+++ common.mk
@@ -270,7 +270,7 @@ PRODUCT_COPY_FILES += \
 
 # Light
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_sm6250
+    android.hardware.light-service.xiaomi
 
 # Media
 PRODUCT_PACKAGES += \
