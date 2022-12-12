919e0a9b17df639e2479f044b240d6c189de75ff
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-12-12 12:31:44 -0500

    sm6250-common: Disable ART debug and optimize dexpreopt
     * minimize java debug info
     * use everything filter for dexpreopt (AOSPA/android_vendor_pa@ab5061c)
     * disable debug configs in art
     * use non-debug dex2oat

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -114,6 +114,12 @@ BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += kpti=off
 
+# ART
+ART_BUILD_TARGET_NDEBUG := true
+ART_BUILD_TARGET_DEBUG := false
+ART_BUILD_HOST_NDEBUG := true
+ART_BUILD_HOST_DEBUG := false
+
 # HIDL
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
