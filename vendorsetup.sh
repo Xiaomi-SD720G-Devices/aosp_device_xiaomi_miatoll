ed18591787df4ac2d8ad6c5731b44e49d386bb46
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2024-04-17 21:08:52 +0530

    miatoll: Add vendorsetup.sh
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,18 @@
+# clang v17.0.4
+git clone -b clang-r498229b https://github.com/Xiaomi-SD720G-Devices/AOSP-clang.git prebuilts/clang/host/linux-x86/clang-r498229b --depth=1
+
+# hardware/dolby
+git clone -b main https://github.com/userariii/hardware_dolby.git hardware/dolby --depth=1
+
+# hardware/sony/timekeep
+git clone -b lineage-21 https://github.com/LineageOS/android_hardware_sony_timekeep.git hardware/sony/timekeep --depth=1
+
+# hardware/xiaomi
+git clone -b lineage-21 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi --depth=1
+
+# kernel/xiaomi
+git clone -b Tiramisu https://github.com/Xiaomi-SD720G-Devices/android_kernel_xiaomi_sm6250.git kernel/xiaomi/sm6250 --depth=1
+
+# vendor/xiaomi
+git clone -b Tiramisu https://github.com/Xiaomi-SD720G-Devices/aosp_vendor_xiaomi_miatoll.git vendor/xiaomi/miatoll --depth=1
+git clone -b Tiramisu https://gitlab.com/userariii/vendor-xiaomi-miuicamera.git vendor/xiaomi/miatoll --depth=1
