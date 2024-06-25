1e673be87a05330bccdb7c2fdb6cbbaed2810ba6
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2024-06-25 21:16:17 +0000

    miatoll: parts: drop! dirac
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -26,7 +26,6 @@ import android.os.IBinder;
 import android.view.Display.HdrCapabilities;
 import android.view.SurfaceControl;
 
-import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 
@@ -38,11 +37,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
-        try {
-        DiracUtils.initialize(context);
-        } catch (Exception e) {
-            Log.d(TAG, "Dirac is not present in system");
-        }
         DozeUtils.checkDozeService(context);
         ThermalUtils.startService(context);
 
